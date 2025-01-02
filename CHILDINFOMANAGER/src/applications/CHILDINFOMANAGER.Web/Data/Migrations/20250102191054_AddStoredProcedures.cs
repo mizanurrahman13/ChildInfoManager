c828f2822c034dfc419ced2286cb24317e51245b
mizanurrahman13 <mizan.441980@gmail.com>
2025-01-03 01:21:15 +0600

    Added Stored Procedure in Migration

---
+++ CHILDINFOMANAGER/src/applications/CHILDINFOMANAGER.Web/Data/Migrations/20250102191054_AddStoredProcedures.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CHILDINFOMANAGER.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoredProcedures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"CREATE PROCEDURE SaveChildren
+@Id NVARCHAR(40),
+@FirstName NVARCHAR(250),
+@LastName NVARCHAR(250),
+@DateOfBirth DateTime,
+@PhoneNumber NVARCHAR(20),
+@HomeAddress NVARCHAR(250)
+AS
+BEGIN
+    DECLARE @ExistingId NVARCHAR(40), @Message NVARCHAR(250);
+    SET @ExistingId = (SELECT Id
+                       FROM [ChildInfoDB].[dbo].[Childrens]
+                       WHERE Id = @Id);
+
+    IF(@ExistingId IS NULL)
+    BEGIN
+        INSERT INTO Childrens (Id, FirstName, LastName, DateOfBirth, PhoneNumber, HomeAddress)
+        VALUES (@Id, @FirstName, @LastName, @DateOfBirth, @PhoneNumber, @HomeAddress);
+
+        SET @Message = 'Children Added Successfully';
+        SELECT @Message AS [Message];
+    END
+    ELSE
+    BEGIN
+        SET @Message = 'Children already exist with this ID: ' + CAST(@Id AS NVARCHAR(50));
+        SELECT @Message AS [Message];
+    END
+END";
+            migrationBuilder.Sql(sql);
+
+            var createGetAllChildrenSP = @"
+                            CREATE PROCEDURE GetAllChildren AS BEGIN SELECT * FROM Childrens; END"; 
+            migrationBuilder.Sql(createGetAllChildrenSP);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            var dropSaveChildrenSP = "DROP PROCEDURE SaveChildren"; 
+            migrationBuilder.Sql(dropSaveChildrenSP); 
+            var dropGetAllChildrenSP = "DROP PROCEDURE GetAllChildren"; 
+            migrationBuilder.Sql(dropGetAllChildrenSP); 
+        }
+    }
+}
